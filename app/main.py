f4d3964a4055b631c7110ae9d4f6191c9c39e7e1
mimfort <mimfort21@gmail.com>
2023-06-05 03:50:49 +0300

    create templates and pages

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as router_pages
 
 
 app = FastAPI()
@@ -12,3 +13,4 @@ app.include_router(router_users)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_bookings)
+app.include_router(router_pages)
