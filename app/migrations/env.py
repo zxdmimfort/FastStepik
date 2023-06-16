e2ed6a48c215f6d6cf78c9c05fa44070e0286440
Mimfort <mimfort21@gmail.com>
2023-06-16 23:28:22 +0300

    edit env.py

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -14,7 +14,7 @@ from app.database import Base
 from app.hotels.models import Hotels
 from app.bookings.models import Bookings
 from app.users.models import Users
-from app.rooms.models import Rooms
+from app.hotels.rooms.models import Rooms
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
